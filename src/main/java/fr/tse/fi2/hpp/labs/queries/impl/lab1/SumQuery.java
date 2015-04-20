89a5004a5b6439158eb2b2978a1c7898b0c6a455
cemTSE <cem.s.yilmaz@gmail.com>
2015-04-20 15:28:27 +0200

    Séance 1

---
+++ src/main/java/fr/tse/fi2/hpp/labs/queries/impl/lab1/SumQuery.java
@@ -0,0 +1,21 @@
+package fr.tse.fi2.hpp.labs.queries.impl.lab1;
+
+import fr.tse.fi2.hpp.labs.beans.DebsRecord;
+import fr.tse.fi2.hpp.labs.beans.measure.QueryProcessorMeasure;
+import fr.tse.fi2.hpp.labs.queries.AbstractQueryProcessor;
+
+public class SumQuery extends AbstractQueryProcessor {
+
+	private float sum = 0; 
+	
+	public SumQuery(QueryProcessorMeasure measure) {
+		super(measure);
+	}
+
+	@Override
+	protected void process(DebsRecord record) {
+		sum += record.getFare_amount();
+		writeLine("Somme " + sum);
+	}
+
+}
